e1fb622916b6f62879e16500293c70b87334fea6
amal <a.ziad@etu.enset-media.ac.ma>
2022-08-05 22:36:18 +0100

    Exercice2 question2

--- src/main/java/ma/enset/exer2/Application2.java
+++ src/main/java/ma/enset/exer2/Application2.java
@@ -1,6 +1,5 @@
 package ma.enset.exer2;
 
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -10,27 +9,35 @@ import scala.Tuple2;
 import java.util.Arrays;
 import java.util.List;
 
-public class Application1 {
+public class Application2 {
     public static void main(String[] args) {
-        SparkConf conf=new SparkConf().setAppName("Exe1 RDD").setMaster("local[*]");
+        SparkConf conf=new SparkConf().setAppName("Exe2 RDD").setMaster("local[*]");
         JavaSparkContext sc=new JavaSparkContext(conf);
         JavaRDD<String> rddLines=sc.textFile("hdfs://localhost:9000/villes.txt");
-        JavaRDD<String> rddNames=rddLines.flatMap(s -> Arrays.asList(s.split("\n")).iterator());
+        JavaRDD<String> rddRows=rddLines.flatMap(s ->
+                Arrays.asList(s.split("\n")).iterator());
 
-        JavaPairRDD<String, Double> rddPairs = rddNames
+        // string annee  13/12/2022
+        String annee="2022";
+        JavaRDD<String> rdddates=rddRows.filter(s ->s.contains(annee));
+        List<String> elems = rddRows.collect();
+        for (String t : elems) {
+            System.out.println(t.toString());
+        }
+        JavaPairRDD<String, Double> rddPairs = rdddates
                 .mapToPair(s -> new Tuple2<>((s.split(" "))[1],
                         Double.valueOf(s.split(" ")[3]) ));
-
         JavaPairRDD<String,Double> ventecount=rddPairs.reduceByKey((a, b) -> a+b);
         // calcul en local
-        List<Tuple2<String, Double>> elems = ventecount.collect();
-        for (Tuple2<String, Double> t : elems) {
+        List<Tuple2<String, Double>>   el = ventecount.collect();
+        for (Tuple2<String, Double> t : el) {
             System.out.println(t.toString());
         }
         // calcul sur le cluster
-        ventecount.saveAsTextFile("hdfs://localhost:9000/venteCount.txt");
-
+        ventecount.saveAsTextFile("hdfs://localhost:9000/venteAnnee.txt");
 
 
     }
+
+
 }
